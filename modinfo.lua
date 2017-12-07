b194aafa8cf7f4c770dfbe1bec818b6acdd8e6b7
Folk <vhaarr@gmail.com>
2017-12-07 17:20:42 +0100

    Set originalUploader, and bump to 1.9.x.

--- modinfo.lua
+++ modinfo.lua
@@ -4,8 +4,9 @@ return {
 	name = "Curator Insight-Curator",
 	tags = { "Utilities", "Diplomacy" },
 	picture = "thumb.png",
-	supported_version = "1.8.*",
+	supported_version = "1.9.*",
 	remote_file_id = 796214744,
+	originalUploader = "folk",
 	readme = "readme.md",
 	steambb = "steam.bbcode",
 }
